16d19efae2357022b15055ed6f4830c71dc4d05f
Talabuddin-Gheyasi <109315284+Talabuddin-Gheyasi@users.noreply.github.com>
2022-07-15 22:39:18 +0430

    Add files via upload

---
+++ 4th Assignment.php
@@ -0,0 +1,23 @@
+<?php
+// This calculator has been created as assignment for presenting to the CodeWeekEnd Instructors.
+//This Assignment is created by Talabuddin Gheyasi, CodeWeekEnd Online Student.
+
+$message="<h3 style='color:blue'>This calculator has been created as assignment for presenting to the CodeWeekEnd Instructors.</h3>";
+echo ($message);
+$number1 = 20;
+$number2 = 10;
+ echo "<br>";
+echo "First Number is: ". $number1; echo "<br>"; 
+ echo "Second number is: ". $number2; 
+$sum = $number1 + $number2; 
+$sub = $number1 - $number2;
+$mul = $number1 * $number2;
+$div = $number1 / $number2; 
+ 
+echo "<br>";
+echo "<br>";
+echo "The Result of sum is is: " . $sum . "<br>";
+echo "The subtraction result of numbers is: " . $sub . "<br>";
+echo "The Multiplication result of numbers is: " . $mul . "<br>";
+echo "The Division of result of numbers is:" . $div;
+ 
