242f347a0aa5decf65a4cf4905abf849486cf595
Gheyasi <109315284+Talabuddin-Gheyasi@users.noreply.github.com>
2022-10-31 10:23:10 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+//$th Assignment for CW online class.
+// }
+
+echo "Enter First number";
+
+echo "<br>". "First number is:  ";
+
+echo $Firt_number=100;
+
+echo "<br>";
+
+echo "Enter second number";
+echo "<br>". "Second number is:  ";
+echo $second_number=50;
+echo "<br>";
+ 
+echo "<br>". "Calculation Result is:  ";
+echo "<br>"."<br>"."<br>";
+
+echo "Sum". ($Firt_number+$second_number) . ".<br>";
+
+echo "Minus: ". ($Firt_number-$second_number) . ".<br>";
+
+echo "Multiply: ". ($Firt_number*$second_number) . ".<br>";
+
+echo "Division: ". ($Firt_number/$second_number) . ".<br>";
+
+?>
